72f00d4b341446eba306fbd21451bbb2b4e859a7
sebas31121 <scm31121@gmail.com>
2024-03-10 10:34:53 -0500

    changes and new functions

--- js/index.js
+++ js/index.js
@@ -1,78 +1,102 @@
-let albumes=[];
+let albumes = [];
 let desplegado;
-document.querySelector(".mas").addEventListener("click",mas);
 
-function mas(){
+document.querySelector(".mas").addEventListener("click", mas);
+
+function mas() {
     const nombre = document.querySelector("#album").value.trim();
-    if(nombre){
-        albumes.push({album:nombre, imagenes:[]});
+    if (nombre) {
+        albumes.push({ album: nombre, imagenes: [] });
         escribir();
     }
 }
 
-function escribir(){
-    document.querySelector(".albumes").innerHTML="";
-    albumes.map((valor,indice)=>{
-        document.querySelector(".albumes").insertAdjacentHTML("beforeend",`
-            <div class="cadaAlmbum" onclick="desplegar(this)" jab="${indice}">
+function escribir() {
+    document.querySelector(".albumes").innerHTML = "";
+    albumes.map((valor, indice) => {
+        document.querySelector(".albumes").insertAdjacentHTML("beforeend", `
+            <div class="cadaAlbum" onclick="desplegar(this)" jab="${indice}">
                 <div class="nombreAlbum" 
-                contenteditable="true"
-                onfocus="activar(this)"
-                onblur="desactivar"(this)"
-                onkeydown="detectarEnter(this,event)">
+                    contenteditable="true"
+                    onfocus="activar(this)"
+                    onblur="desactivar(this)"
+                    onkeydown="detectarEnter(this, event)">
                     ${valor.album}
                 </div>
             </div>
-        `)
-    })
+        `);
+    });
 }
 
-function activar(){
-
+function activar() {
+    // Lógica para activar la edición del nombre del álbum
 }
 
-function desactivar(){
-
+function desactivar() {
+    // Lógica para desactivar la edición del nombre del álbum
 }
 
-function detectarEnter(){
-
+function detectarEnter(elemento, evento) {
+    // Lógica para detectar la tecla Enter durante la edición del nombre del álbum
 }
 
-function desplegar(yo){
-    document.querySelector(".miAlbum").style.dislay="block";
-    desplegado=(yo.getAttribute("jab"));
-    const datos= albumes[desplegado];
-    document.querySelector(".miAlbum").innerHTML=`
+function desplegar(yo) {
+    document.querySelector(".miAlbum").style.display = "block";
+    desplegado = yo.getAttribute("jab");
+    const datos = albumes[desplegado];
+    document.querySelector(".miAlbum").innerHTML = `
         <h1>${datos.album}</h1>
         <div class="imagenes"></div>
         <div class="caja">
             <input type="file" name="fichero" id="fichero" accept="image/*"/>
-            <button id="enviar" onclick="enviar()">Subir fichero </button>
+            <button id="enviar" onclick="enviar()">Subir fichero</button>
         </div>`;
-    if(datos.imagenes.length>0){
-        datos.imagenes.map((valor)=>{
-            //Dibuje las imagenes
-        })
-    }
-    function enviar(){
-        if(fichero.files.length>0){
-            let data = new FormData();
-            data.append('fichero',fichero.files[0]);
-            fetch('php/subir.php',{
-                method:'POST',
-                body:data
-            })
-            .then(response => response.text())
-            .then(data => {
-                albumes[desplegado].imagenes.push(data.trim());
-                codigoHTML(data);
-                escribir();
-            })
-        }
+    if (datos.imagenes.length > 0) {
+        datos.imagenes.map((valor) => {
+            // Dibujar las imágenes
+        });
     }
 }
 
-function codigoHTML(){
-    
+function enviar() {
+    const fichero = document.getElementById('fichero');
+    if (fichero.files.length > 0) {
+        let data = new FormData();
+        data.append('fichero', fichero.files[0]);
+        fetch('php/subir.php', {
+            method: 'POST',
+            body: data
+        })
+        .then(response => response.text())
+        .then(data => {
+            albumes[desplegado].imagenes.push(data.trim());
+            codigoHTML(data);
+            escribir();
+        });
+    }
+}
+
+function codigoHTML(dato) {
+    document.querySelector(".imagenes").insertAdjacentHTML("beforeend",`
+    <div class="imagen"
+        onmouseover="mostrar(this)"
+        onmouseout="ocultar(this"
+        onclick="ampliar('${dato}')" alt=""/>
+        <img src="${dato}" alt=""/>
+        <img class="papelera" src=../img/papelera.png" onclick="eliminarImagen(this,'${dato}',event)"/>
+    </div>
+    `)
+}
+
+function mostrar(yo){
+    if(yo.querySelector(".papelera").style.display==="block"){
+        yo.querySelector(".papelera").style.display="none";
+    }else{
+        yo.querySelector(".papelera").style.display="block"
+    }
+}
+
+function ampliar(miImagen){
+    document.querySelector(".ampliacion").style.display="block";
+    document.querySelector(".imagenGrande").innerHTML=`<img src="${miImagen}"/>`;
 }
--- php/subir.php
+++ php/subir.php
@@ -1,28 +1,32 @@
 <?php
 
-$carpeta="../ficheros/";
-$nombreOriginal=basename($_FILES["ficheros"]["name"]);
-$uploadOK=1;
-$formatoImagen=strtolower(pathinfo($nombreOriginal,PATHINFO_EXTENSION));
-$nombreUnico=md5(uniqid());
-$nombreFinal=$carpeta.$nombreUnico.".".$formatoImagen;
-$imagenAMostrar="ficheros/".$nombreUnico.".".$formatoImagen;
+error_reporting(E_ALL);
+ini_set('display_errors', '0');
 
-$check= getimagesize($_FILES["fichero"]["tmp_name"]);
-if($check === false){
+$carpeta = "../ficheros/";
+$nombreOriginal = basename($_FILES["fichero"]["name"]);
+$uploadOK = 1;
+$formatoImagen = strtolower(pathinfo($nombreOriginal, PATHINFO_EXTENSION));
+$nombreUnico = md5(uniqid());
+$nombreFinal = $carpeta . $nombreUnico . "." . $formatoImagen;
+$imagenAMostrar = "ficheros/" . $nombreUnico . "." . $formatoImagen;
+
+$check = getimagesize($_FILES["fichero"]["tmp_name"]);
+if ($check === false) {
     echo "El archivo no es una imagen";
-    $uploadOK=0;
+    $uploadOK = 0;
 }
 
-if($_FILES["fichero"]["size"]>10000000){
+if ($_FILES["fichero"]["size"] > 10000000) {
     echo "El archivo es muy grande";
-    $uploadOK=0;
+    $uploadOK = 0;
 }
 
-if($uploadOK === 1){
-    if(move_uploaded_file($_FILES["fichero"]["tmp_name"],$nombreFinal)){
+if ($uploadOK === 1) {
+    if (move_uploaded_file($_FILES["fichero"]["tmp_name"], $nombreFinal)) {
         echo $imagenAMostrar;
-    }else{
-        echo "lo siento, hubo un error";
+    } else {
+        echo "Lo siento, hubo un error";
     }
 }
+?>
